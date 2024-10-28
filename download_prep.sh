f695d5b4a2cb3f979f1588b510469e75f7d59523
Jon Ludlam <jon@recoil.org>
2024-10-28 10:19:42 +0000

    better prep

--- download_prep.sh
+++ download_prep.sh
@@ -10,7 +10,7 @@ pkg=$3    # prep0/u/745d6c5e6ba222bcc493e3bdedc73465/cmdliner/1.3.0/
 cd /home/opam/.cache/prep
 
 
-if [ ! -f $pkg/content.tar ]; then
+if [ ! -d $pkg/home ]; then
    mkdir -p $pkg
    RC=1 
    while [[ $RC -ne 0 ]]
@@ -18,9 +18,25 @@ if [ ! -f $pkg/content.tar ]; then
       rsync -av $host:$dir/$pkg/content.tar $pkg/
       RC=$?
    done
+   pushd $pkg
+   tar xf content.tar
+   rm -f content.tar
+   popd
 fi
 
-tar -C / -xf $pkg/content.tar
+cd $pkg
+rm -f content.tar
+
+for dir in $(find . -type d); do
+  mkdir -p /$dir
+done
+
+pwd=$(pwd)
+for file in $(find . -type f); do
+  ln -s $pwd/$file /$file || true
+done
+
+
 
 
 
